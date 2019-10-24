eab977470f807b298f7c85620c29eef4e84bf4d7
Tarun Talreja <taruntalreja08@gmail.com>
2019-10-24 11:50:51 +0530

    changes in consumer server

--- consumer_server.py
+++ consumer_server.py
@@ -2,17 +2,19 @@ from pykafka import KafkaClient
 from pykafka.simpleconsumer import OffsetType
 import logging
 
-logging.getLogger("pykafka.broker").setLevel('ERROR')
+logging.getLogger("pykafka.broker").setLevel('ERROR') 
 
-client = KafkaClient(hosts="localhost:9092")
-topic = client.topics[b'service-calls']
+client = KafkaClient(hosts="localhost:9092") 
+topic = client.topics["service-calls"] 
+
 consumer = topic.get_balanced_consumer(
-    consumer_group=b'pytkafka-test-2',
-    auto_commit_enable=False,
+    consumer_group = b'pytkafka-test-2',
+    auto_commit_enable = False,
     auto_offset_reset=OffsetType.EARLIEST,
-    zookeeper_connect='remotehost:2181'
+    zookeeper_connect = 'localhost:2181'
 )
+
+#Print out the messages
 for message in consumer:
     if message is not None:
-        print(message.offset, message.value)
-        
+        print(message.value.decode('utf-8'))
